6259e06c11f0adaeda6994c17ab91cba4f00ff29
Dave Menninger <dave.menninger@gmail.com>
2014-11-12 23:48:02 -0500

    add mango/auth test

--- lib/Lrrr/Authentication.pm
+++ lib/Lrrr/Authentication.pm
@@ -3,11 +3,7 @@ package Lrrr::Authentication;
 sub load_user {
   my ($class, $app, $username) = @_;
         
-  #return 1;
-  #my $self = shift;
-  #my $username = shift;
-
-  my $collection = $app->mango->db('test')->collection('users');
+  my $collection = $app->mango->db->collection('users');
   my $user = $collection->find_one( {username => $username} );
 
   return { 
@@ -19,16 +15,10 @@ sub load_user {
 sub validate_user {
   my ($class, $app, $username, $password, $extas) = @_;
 
-  #return 1;
-  #my $self = shift;
-  #my $username = shift || '';
-  #my $password = shift || '';
-  #my $extradata = shift || {};
-
-  my $collection = $app->mango->db('test')->collection('users');
+  my $collection = $app->mango->db->collection('users');
   my $user = $collection->find_one( {username => $username} );
 
-  return $user->{username} if ( $password eq $user->{password} );
+  return $user->{username} if ( defined $user->{username} && defined $user->{password} && $password eq $user->{password} );
   return undef;
 }
 
